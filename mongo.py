605612685cdf2ea5768d6d454cc2c4837f38992c
Jakub Czyzewski <jakub.czyzewski@u2i.com>
2015-06-14 19:09:00 +0200

    Add location&persons pair csv extraction

--- mongo.py
+++ mongo.py
@@ -1,4 +1,5 @@
 import pymongo
+import unicodecsv
 from bson.code import Code
 
 
@@ -11,7 +12,7 @@ class Mongo:
 	def getCollection(self):
 		return self.collection
 
-	def insertCollection(self, list: list):
+	def insertCollection(self, list):
 		for item in list:
 			self.insertItem(item)
 
@@ -42,6 +43,27 @@ class Mongo:
 					continue
 				self.collection.delete_one({"_id":object})
 		print(len(result))
+
+	def get_locations_pairs(self):
+		pairs = []
+		with open('locations_pars.csv', 'w') as out:
+			csv_out = unicodecsv.writer(out)
+			for post in self.collection.find({"locations": {"$not": {"$size": 0}}}, {"locations": 1} ):
+				for i in range(0,len(post['locations'])):
+					for j in range(i+1,len(post['locations'])):
+						pairs.append( (post['locations'][i],post['locations'][j]))
+						csv_out.writerow([post['locations'][i],post['locations'][j]])
+
+	def get_persons_pairs(self):
+		pairs = []
+		with open('persons_pars.csv', 'w') as out:
+			csv_out = unicodecsv.writer(out)
+			print self.collection.find({"persons": {"$not": {"$size": 0}},"published": {'$regex': '2015-05-0[1,2,3,4,5,6,7]*'}}, {"persons": 1} ).count()
+			for post in self.collection.find({"persons": {"$not": {"$size": 0}},"published": {'$regex': '2015-05-0[1,2,3,4,5,6,7]*'}}, {"persons": 1} ):				
+				for i in range(0,len(post['persons'])):
+					for j in range(i+1,len(post['persons'])):
+						pairs.append( (post['persons'][i],post['persons'][j]))
+						csv_out.writerow([post['persons'][i],post['persons'][j]])						
 
 	def removeNonEnglishArticles(self):
 		print(self.collection.count())
